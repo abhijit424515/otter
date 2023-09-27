fd681cc67267f6453a5432e8ce1ac56d9ade4b57
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2023-09-27 16:51:34 +0530

    header, footer, basic ui improved

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				rubrik: ["Rubrik", "sans-serif"],
+				nce: ["Noto Color Emoji", "Inter", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
-}
+};
